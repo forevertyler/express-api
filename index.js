0a5e414934f6037575944b1e6e26e7c48e38ea0e
forevertyler <1318719240@qq.com>
2016-09-27 16:20:10 +0800

    2ed

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  var page = "<html>" +
-                "<body>" +
-                  "<h1>INDEX.HTML</h1>" +
-                "</body>" +
-              "</html>"
-  res.send(page)
-})
-app.get('/about.html', function (req, res) {
+// app.get('/', function (req, res) {
+//   var page = "<html>" +
+//                 "<body>" +
+//                   "<h1>INDEX.HTML</h1>" +
+//                 "</body>" +
+//               "</html>"
+//   res.send(page)
+// })
+app.get('/:nameself', function (req, res) {
+  var username = req.params.nameself;
   var about = "<html>" +
                 "<body>" +
-                  "<h1>ABOUT.HTML</h1>" +
+                  "<h1>"+
+                   username+
+                  "</h1>" +
                 "</body>" +
               "</html>"
   res.send(about)
